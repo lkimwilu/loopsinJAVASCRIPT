8ff937a3b167fc5b1e6c218b062035abb05f02e1
lkimwilu <lkimwilu@gmail.com>
2024-07-15 13:04:46 +0300

    loop.js

---
+++ loop.js
@@ -0,0 +1,69 @@
+// for (var i = 0; i< 5; i++) {
+//     console.log("for loop iteration: " + i);
+//     i++;
+// }
+
+
+ WHILE loop iteration
+
+var i = 0;
+while (i < 5) {
+    console.log("while loop iteration: " + i);
+    i++;
+}
+
+
+
+//  DO-WHile loop iteration
+// Executes code of block then checks condition later
+// var i = 100;
+// do {
+//     console.log("do-while loop iteration: " + i);
+//     i++;
+// } while (i < 5);
+
+
+
+// CONTROLFLOW INTERRUPTIONS
+// EXAMPLE of break
+// for (var i = 0; i < 10; i++) {   
+//     if (i ==5) {
+//         break; 
+//     }
+//     console.log("loop with break: " + i);
+// }
+// 2.using continue statement
+//  for (var i = 0; i < 10; i++){
+//     if (i % 2 === 0) {
+//         continue;
+//     }
+//     console.log("loop with continue: " + i);
+// }
+// 3.using return statement
+// function add(a, b) {
+//     return a + b;
+// }
+// let a = 1, b = 2
+//  console.log(add(a, b));
+
+
+// CONTINUE statement
+// for  (i = 0; i < 10; i++) {
+//     if (i % 2=== 1) {
+//         continue;
+//     }
+//     console.log("loop with continue: " + i);
+// }
+
+
+
+
+// for (var i = 0; i < 3; i++) {
+
+//     for (var j = 0; j < 3; j++) {
+
+//         if (j===1)
+//         continue;
+//     console.log(`i = ${i}, j = ${j}`);
+//     }
+// }
