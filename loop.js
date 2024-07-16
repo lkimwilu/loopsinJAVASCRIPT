49d89d8cd407f3ffda57fab6a6e56780d2c0ab32
lkim <lkimwilu@gmail.com>
2024-07-16 17:27:32 +0300

    loops assignment

--- loop.js
+++ loop.js
@@ -69,11 +69,18 @@
 // }
 
 
- var i = 0; 
- while (i < 20){
-    if (i % 2 ===0) 
-        continue;{
-        console.log("loop with continue: " + i);
+//  var i = 0; 
+//  while (i < 20){
+//     if (i % 2 ===0) 
+//         continue;{
+//         console.log("loop with continue: " + i);
+//         }
+//         i++;
+//     }
+
+    for (var i = 0; i < 20; i++) {
+        if (i % 2 === 0) {
+            continue;  
         }
-        i++;
+        console.log("loop with continue: " + i);  
     }
